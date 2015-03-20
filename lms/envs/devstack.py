93328f4152dbfc8e9129ea6046847d169a158b15
Zia Fazal <zia.fazal@arbisoft.com>
2015-03-20 17:53:42 +0500

    making things to work after rebase

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -32,7 +32,16 @@ FEATURES['ENABLE_INSTRUCTOR_EMAIL'] = True     # Enable email for all Studio cou
 FEATURES['REQUIRE_COURSE_EMAIL_AUTH'] = False  # Give all courses email (don't require django-admin perms)
 
 
-########################## ANALYTICS TESTING ########################
+############# Performance Profiler #################
+# Note: The Django Debug Toolbar creates a lot of profiling noise, so
+# when the profiler is enabled in Devstack we should also disable the toolbar
+FEATURES['PROFILER'] = False
+if FEATURES.get('PROFILER'):
+    INSTALLED_APPS += ('profiler',)
+    MIDDLEWARE_CLASSES += (
+        'profiler.middleware.HotshotProfilerMiddleware',
+        'profiler.middleware.CProfileProfilerMiddleware',
+    )
 
 ANALYTICS_SERVER_URL = "http://127.0.0.1:9000/"
 ANALYTICS_API_KEY = ""
@@ -40,47 +49,40 @@ ANALYTICS_API_KEY = ""
 # Set this to the dashboard URL in order to display the link from the
 # dashboard to the Analytics Dashboard.
 ANALYTICS_DASHBOARD_URL = None
-
-
 ################################ DEBUG TOOLBAR ################################
+FEATURES['DEBUG_TOOLBAR'] = True
+if FEATURES.get('DEBUG_TOOLBAR'):
+    INSTALLED_APPS += ('debug_toolbar', 'debug_toolbar_mongo')
+    MIDDLEWARE_CLASSES += (
+        'django_comment_client.utils.QueryCountDebugMiddleware',
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    INTERNAL_IPS = ('127.0.0.1',)
 
-INSTALLED_APPS += ('debug_toolbar', 'debug_toolbar_mongo')
-MIDDLEWARE_CLASSES += ('django_comment_client.utils.QueryCountDebugMiddleware',
-                       'debug_toolbar.middleware.DebugToolbarMiddleware',)
-INTERNAL_IPS = ('127.0.0.1',)
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+        'debug_toolbar_mongo.panel.MongoDebugPanel',
 
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-    'debug_toolbar_mongo.panel.MongoDebugPanel',
-
-    #  Enabling the profiler has a weird bug as of django-debug-toolbar==0.9.4 and
-    #  Django=1.3.1/1.4 where requests to views get duplicated (your method gets
-    #  hit twice). So you can uncomment when you need to diagnose performance
-    #  problems, but you shouldn't leave it on.
-    #'debug_toolbar.panels.profiling.ProfilingDebugPanel',
-)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-    'SHOW_TOOLBAR_CALLBACK': lambda _: True,
-}
-
-INSTALLED_APPS += (
-    # Mongo perf stats
-    'debug_toolbar_mongo',
+        #  Enabling the profiler has a weird bug as of django-debug-toolbar==0.9.4 and
+        #  Django=1.3.1/1.4 where requests to views get duplicated (your method gets
+        #  hit twice). So you can uncomment when you need to diagnose performance
+        #  problems, but you shouldn't leave it on.
+        #  'debug_toolbar.panels.profiling.ProfilingPanel',
     )
 
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'SHOW_TOOLBAR_CALLBACK': lambda _: True,
+    }
 
-DEBUG_TOOLBAR_PANELS += (
-   'debug_toolbar_mongo.panel.MongoDebugPanel',
-   )
+
 
 ########################### PIPELINE #################################
 
@@ -100,14 +102,6 @@ CC_PROCESSOR = {
         "PROFILE_ID": 'edx',
     }
 }
-
-########################### EDX API #################################
-
-FEATURES['API'] = True
-
-########################## USER API ########################
-EDX_API_KEY = None
-
 
 ########################### External REST APIs #################################
 FEATURES['ENABLE_MOBILE_REST_API'] = True
@@ -131,6 +125,14 @@ FEATURES['MILESTONES_APP'] = True
 FEATURES['ENTRANCE_EXAMS'] = True
 
 
+########################### EDX API #################################
+
+FEATURES['API'] = True
+
+########################## USER API ########################
+EDX_API_KEY = None
+
+
 #####################################################################
 # See if the developer has any local overrides.
 try:
@@ -141,3 +143,14 @@ except ImportError:
 #####################################################################
 # Lastly, run any migrations, if needed.
 MODULESTORE = convert_module_store_setting_if_needed(MODULESTORE)
+
+########################## SECURITY #######################
+
+FEATURES['ENFORCE_PASSWORD_POLICY'] = False
+FEATURES['ENABLE_MAX_FAILED_LOGIN_ATTEMPTS'] = False
+FEATURES['SQUELCH_PII_IN_LOGS'] = False
+FEATURES['PREVENT_CONCURRENT_LOGINS'] = False
+FEATURES['ADVANCED_SECURITY'] = False
+
+PASSWORD_MIN_LENGTH = None
+PASSWORD_COMPLEXITY = {}
