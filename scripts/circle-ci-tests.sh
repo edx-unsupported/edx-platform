c6ab53653283b3b06dc08787d85cb8002b5be079
Zia Fazal <zia.fazal@arbisoft.com>
2016-08-29 20:13:00 +0500

    update solutions app hash and jvm settings

--- scripts/circle-ci-tests.sh
+++ scripts/circle-ci-tests.sh
@@ -85,9 +85,9 @@ else
             paver test_lib --extra_args="--with-flaky" --cov_args="-p" --with-xunitmp
             paver test_system -s lms -t edx_solutions_organizations
             paver test_system -s lms -t gradebook
-            #paver test_system -s lms -t progress
+            paver test_system -s lms -t progress
             paver test_system -s lms -t edx_solutions_projects
-            #paver test_system -s lms -t course_metadata
+            paver test_system -s lms -t course_metadata
             paver test_system -s lms -t social_engagement
             paver test_system -s lms -t edx_solutions_api_integration
             ;;
